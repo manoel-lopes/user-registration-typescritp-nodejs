308b95fb6bd5befdf37cda8e44c4aa91e47a4fd3
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-09 11:00:03 -0300

    creating the routes to create and update a user

--- src/routes.ts
+++ src/routes.ts
@@ -8,8 +8,8 @@ const userController = new UserController()
 
 // routes.get('/users', userController.index)
 // routes.get('/users/:id', userController.show)
-// routes.post('/users', userController.store)
-// routes.put('/users/:id', userController.update)
+routes.post('/users', userController.store)
+routes.put('/users/:id', userController.update)
 // routes.delete('/users/:id', userController.delete)
 
 export { routes }
