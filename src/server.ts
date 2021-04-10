208da6223f17fb3a80c16eb4a7a744993af3e1b6
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-10 18:25:33 -0300

    adding cors into the project

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata'
 import express from 'express'
 import { routes } from './routes'
+import cors from 'cors'
 
 import './db'
 
 const app = express()
 const port = 3001
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
