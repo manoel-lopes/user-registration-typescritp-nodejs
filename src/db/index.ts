4968e469d50c1bd5b334f127e55205ac23218de6
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-11 15:03:39 -0300

    Revert "moving the settings from ormconfig to createConnection"
    This reverts commit 2503c15876477947a0ee4725671e3dc4ade14606.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,3 @@
 import { createConnection } from 'typeorm'
 
-createConnection({
-  type: 'sqlite',
-  database: './src/db/db.sqlite',
-  entities: ['./src/app/models/**.ts'],
-  migrations: ['./src/db/migrations/**.ts'],
-  cli: {
-    migrationsDir: './src/db/migrations'
-  }
-})
+createConnection()
