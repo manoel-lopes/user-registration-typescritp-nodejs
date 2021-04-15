2a0fa61a1f0fe6f8236f3da044fbad92a705aee8
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-15 00:05:12 -0300

    changing the tests database from sqlite to postgres

--- src/__tests__/integration/User.test.ts
+++ src/__tests__/integration/User.test.ts
@@ -1,8 +1,9 @@
 import request from 'supertest'
+import { getConnection } from 'typeorm'
 import { v4 as uuid } from 'uuid'
 
-import { app } from '../app'
-import createConnection from '../db'
+import { app } from '../../app'
+import createConnection from '../../db'
 
 describe('Users', () => {
   beforeAll(async() => {
@@ -10,6 +11,12 @@ describe('Users', () => {
     await conn.runMigrations()
   })
 
+  afterAll(async() => {
+    const conn = getConnection()
+    await conn.dropDatabase()
+    await conn.close()
+  })
+  
   // STORE
   
   it('Should not be able to create a new user without pass name', async() => {
--- src/db/index.ts
+++ src/db/index.ts
@@ -4,14 +4,12 @@ export default async(): Promise<Connection> => {
   const isNodeEnvSet = process.env.NODE_ENV
   const defaultOptions = await getConnectionOptions()
 
-  const type = isNodeEnvSet ? 'sqlite' : defaultOptions.type
   const database = isNodeEnvSet 
-    ? './src/__tests__/db_test.sqlite'
+    ? process.env.TESTS_DB_NAME
     : defaultOptions.database
 
   return createConnection(
     Object.assign(defaultOptions, {
-      type,
       database
     })
   )
